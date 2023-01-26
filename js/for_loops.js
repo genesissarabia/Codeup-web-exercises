a4819018e722c8495bda00fd181c1a75b82d68ce
Genesis Sarabia <genesissarabia14@gmail.com>
2023-01-26 16:28:48 -0600

    done with for loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -11,6 +11,7 @@ function showMultiplicationTable(x) {
 
 
 
+
 function randomNumber(){
     return Math.floor(Math.random() * 200 + 20);
 }
@@ -29,3 +30,24 @@ function randomOddOrEven () {
 
 randomOddOrEven();
 
+
+//will i have to use a string that adds a 1 to each multiplier? bc it seems
+// like it's multiplying by 1, 11, 111, 1111, and so on..
+//
+var t=9;
+var string="";
+
+    for (var u = 1; u <= t; u++) {
+        for (var v=1; v<=u; v++){
+            string += u;
+        }
+    string += "\n"
+
+}
+console.log(string);
+
+// I need multiples of 5 and I need them to go down by 5 from 100.
+
+    for (var i=100; i>=5; i-=5){
+        console.log(i);
+    }
