b373bfb722e7b59e9f36f64ec263206a22783c09
Genesis Sarabia <genesissarabia14@gmail.com>
2023-01-26 14:19:59 -0600

    20-200 odd or even loop random number generator exercise complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,11 +1,31 @@
 "use strict";
 
-var x =  7;
+/*var x =  7;
 
 function showMultiplicationTable(x) {
     for (var y = 0; y <= 10; y++) {
         var z=x*y;
         console.log("7 x " + y  +"= " + z);
     }
+}*/
+
+
+
+function randomNumber(){
+    return Math.floor(Math.random() * 200 + 20);
 }
+function isOddOrEven(a) {
+        if (a % 2 === 0) {
+            console.log(a + " is even.");
+        } else {
+            console.log(a + " is odd.");
+        }
+}
+function randomOddOrEven () {
+    for (var i = 0; i < 10; i++) {
+        isOddOrEven(randomNumber());
+    }
+}
+
+randomOddOrEven();
 
