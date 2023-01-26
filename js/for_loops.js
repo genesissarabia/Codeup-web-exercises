b8733dd052c20bdda5f5a14d9ab5b3881c615bc3
Genesis Sarabia <genesissarabia14@gmail.com>
2023-01-26 10:09:45 -0600

    for loops exercise completed

---
+++ js/for_loops.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var x =  7;
+
+function showMultiplicationTable(x) {
+    for (var y = 0; y <= 10; y++) {
+        var z=x*y;
+        console.log("7 x " + y  +"= " + z);
+    }
+}
+
