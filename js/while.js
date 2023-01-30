ad5d8fee06ba02cc9dafffbc75f072d00c91b211
Genesis Sarabia <genesissarabia14@gmail.com>
2023-01-29 19:26:30 -0600

    ice cream exercise conditions met

--- js/while.js
+++ js/while.js
@@ -8,4 +8,29 @@ while (i<=65000){
     i++;
 }
 
+//do-while loop
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+function iceCream(allCones) {
+
+    do {
+        console.log(allCones)
+        var conesSoldToCustomer = Math.floor(Math.random() * 5) + 1;
+        var allCones = allCones - conesSoldToCustomer;
+        console.log(allCones)
+        console.log(conesSoldToCustomer + " cones sold to customer.")
+    } while (allCones > conesSoldToCustomer);
+
+        if (allCones<conesSoldToCustomer){
+            console.log("Cannot sell you " + conesSoldToCustomer + ", I" +
+                " only have " + allCones)
+        }
+}
+        if (allCones === 0){
+            console.log("Yay, I sold them all!")
+}
+
+iceCream(allCones)
 
