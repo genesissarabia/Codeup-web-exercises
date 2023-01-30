dbe98176632a74144beac7f5a2f984550df551b0
Genesis Sarabia <genesissarabia14@gmail.com>
2023-01-30 12:34:22 -0600

    completed ice cream exercise

--- js/while.js
+++ js/while.js
@@ -18,19 +18,20 @@ function iceCream(allCones) {
     do {
         console.log(allCones)
         var conesSoldToCustomer = Math.floor(Math.random() * 5) + 1;
-        var allCones = allCones - conesSoldToCustomer;
-        console.log(allCones)
         console.log(conesSoldToCustomer + " cones sold to customer.")
-    } while (allCones > conesSoldToCustomer);
-
-        if (allCones<conesSoldToCustomer){
+        if (allCones < conesSoldToCustomer) {
             console.log("Cannot sell you " + conesSoldToCustomer + ", I" +
                 " only have " + allCones)
+        } else {
+            allCones -= conesSoldToCustomer;
+            console.log(conesSoldToCustomer + " have been sold.");
+            console.log(allCones + " left.")
         }
-}
-        if (allCones === 0){
-            console.log("Yay, I sold them all!")
+
+    } while (allCones > 0);
+    console.log("Yay, I sold them all!")
 }
 
-iceCream(allCones)
+
+iceCream(allCones);
 
