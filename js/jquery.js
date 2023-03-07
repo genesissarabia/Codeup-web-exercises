802fc12b8c2b1a343c4becff32270244db4f6984
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-07 15:20:50 -0600

    alert completed

---
+++ js/jquery.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userInput = "";
+//
+// var upArrow = (e.keyCode === 38);
+// var downArrow = (e.keyCode === 40);
+// var leftArrow = (e.keyCode === 37);
+// var rightArrow = (e.keyCode === 39);
+// var enterKey = (e.keyCode === 13);
+
+
+
