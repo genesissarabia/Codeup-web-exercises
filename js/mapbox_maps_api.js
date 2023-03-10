35769c844df893fdfdeea15fbe2e979958f0eead
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-10 16:11:22 -0600

    added geocoding js file and corrected to retrieve info via geocode

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,13 +1,4 @@
 "use strict"
 
-let myMarker = new mapboxgl.Marker();
-myMarker.setLngLat([-98.48503860638156, 29.56599326856003]);
-myMarker.addTo(map);
 
-let popup = new mapboxgl.Popup();
-
-popup.setLngLat([-98.48503860638156, 29.56599326856003])
-popup.setHTML("<span>El Bucanero</span>");
-
-myMarker.setPopup(popup);
 
