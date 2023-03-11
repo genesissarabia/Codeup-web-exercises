66e54990f2a47ef892728dd209ee203571dc50a7
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-10 22:44:49 -0600

    completed adding markers from json file

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -2,3 +2,14 @@
 
 
 
+// $.get("data/mapbox_maps_api.json").done(function(data){
+//     let html = "";
+//
+//     data.forEach(function(restaurant){
+//         html += "<h1>"+ restaurant.name + "</h1>"
+//             + "<span>" + restaurant.type + "</span>"
+//             + "<span>" + restaurant.favorite + "</span>"
+//             + "<span>" + restaurant.category + "</span>"
+//     })
+//     $("#restaurants").append(popup);
+// })
