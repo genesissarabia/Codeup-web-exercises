cb2f963bfbfc2d603021cb0a061d59b5e4204517
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-15 01:12:15 -0500

    added background imagery and color styling to cards

--- js/weather_map.js
+++ js/weather_map.js
@@ -17,11 +17,11 @@ function geoCodeBuildForecast(searchString) {
             for (var i = 0; i <= 39; i += 8) {
 
 
-                html += `<div class="col-2 card">`
+                html += `<div class="col-2 card mx-2" style="color: #F0F0F0">`
 
                 html += `<img class='icons card-img-top' src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`
 
-                html += `<div class="card-body">`
+                html += `<div class="card-body" style="background: #A4DDED">`
 
                 html += `<h4 class="card-title">Date: ${data.list[i].dt_txt}</h4>`;
 
@@ -78,31 +78,42 @@ const marker = new mapboxgl.Marker({
     marker.addTo(map);
 
 marker.on("dragend", function(e) {
+     let html="";
+
     let coordinates = e.target._lngLat;
     reverseGeocode({lat: coordinates.lat, lng: coordinates.lng}, mapBoxAppId).then(function (results) {
-        console.log(results)
 
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherAppId}&units=imperial`).done(function (data){
 
-            html="";
 
             for (var i = 0; i <= 39; i += 8) {
 
-                html += `<h4>Date: ${data.list[i].dt_txt}</h4>`;
+                html += `<div class="col-2 card mx-2 text-light">`
 
-                html += `<h5>Min & Max: ${data.list[i].main.temp_min}&deg; / ${data.list[0].main.temp_max}&deg;</h5>`;
+                html += `<img class='icons card-img-top' src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`
 
-                html += `<img class='icons' src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`
+                html += `<div class="card-body" style="background: #A4DDED">`
 
-                html += `<h5>Description: ${data.list[i].weather[0].description}</h5>`;
+                html += `<h4 class="card-title">Date: ${data.list[i].dt_txt}</h4>`;
 
-                html += `<h5>Humidity: ${data.list[i].main.humidity}</h5>`;
+                html += `<p class="card-text">Min & Max: ${data.list[i].main.temp_min}&deg; / ${data.list[0].main.temp_max}&deg;</p>`;
 
-                html += `<h5>Wind: ${parseInt(data.list[i].wind.speed)}</h5>`;
 
-                html += `<h5>Pressure: ${data.list[i].main.pressure}</h5>`;
+                html += `<p class="card-text">Description: ${data.list[i].weather[0].description}</p>`;
+
+                html += `<p class="card-text">Humidity: ${data.list[i].main.humidity}</p>`;
+
+                html += `<p class="card-text"Wind: ${parseInt(data.list[i].wind.speed)}</p>`;
+
+                html += `<p class="card-text">Pressure: ${data.list[i].main.pressure}</p>`;
+
+                html += `</div>` //card-body
+
+                html += `</div>` //card
+
+                html += `</div>` //column
+
             }
-
             $("#weatherBody").html(html);
 
         })
