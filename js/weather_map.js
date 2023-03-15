4074345eb9d7169bb520f95c89fe27e8cfed61ce
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-15 00:26:07 -0500

    cards aligned

--- js/weather_map.js
+++ js/weather_map.js
@@ -9,25 +9,41 @@ function geoCodeBuildForecast(searchString) {
         map.flyTo(markerFlyTo);
         marker.setLngLat(results1)
 
+        html += `<div class="row">`
+
+
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results1[1]}&lon=${results1[0]}&appid=${openWeatherAppId}&units=imperial`).done(function (data) {
-            console.log(data)
 
             for (var i = 0; i <= 39; i += 8) {
 
-                html += `<h4>Date: ${data.list[i].dt_txt}</h4>`;
 
-                html += `<h5>Min & Max: ${data.list[i].main.temp_min}&deg; / ${data.list[0].main.temp_max}&deg;</h5>`;
+                html += `<div class="col-2 card">`
 
-                html += `<img class='icons' src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`
+                html += `<img class='icons card-img-top' src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`
 
-                html += `<h5>Description: ${data.list[i].weather[0].description}</h5>`;
+                html += `<div class="card-body">`
 
-                html += `<h5>Humidity: ${data.list[i].main.humidity}</h5>`;
+                html += `<h4 class="card-title">Date: ${data.list[i].dt_txt}</h4>`;
 
-                html += `<h5>Wind: ${parseInt(data.list[i].wind.speed)}</h5>`;
+                html += `<p class="card-text">Min & Max: ${data.list[i].main.temp_min}&deg; / ${data.list[0].main.temp_max}&deg;</p>`;
 
-                html += `<h5>Pressure: ${data.list[i].main.pressure}</h5>`;
+
+                html += `<p class="card-text">Description: ${data.list[i].weather[0].description}</p>`;
+
+                html += `<p class="card-text">Humidity: ${data.list[i].main.humidity}</p>`;
+
+                html += `<p class="card-text"Wind: ${parseInt(data.list[i].wind.speed)}</p>`;
+
+                html += `<p class="card-text">Pressure: ${data.list[i].main.pressure}</p>`;
+
+                html += `</div>` //card-body
+
+                html += `</div>` //card
+
+                html += `</div>` //column
+
             }
+            html += `</div>`
 
             $("#weatherBody").html(html);
 
