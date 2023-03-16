84e6a3a560d631b090d68ae7633b4c763ffa190e
Genesis Sarabia <genesissarabia14@gmail.com>
2023-03-16 02:34:14 -0500

    satisfied with project styling, believing ready for presentation

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,8 +3,7 @@
 function geoCodeBuildForecast(searchString) {
     let html = "";
     geocode(searchString, mapBoxAppId).then(function (results1) {
-
-
+        // FLIES TO SEARCHED LOCATION
         let markerFlyTo = {center: results1, zoom: 12}
         map.flyTo(markerFlyTo);
         marker.setLngLat(results1)
@@ -12,13 +11,14 @@ function geoCodeBuildForecast(searchString) {
         html += `<div class="row">`
 
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results1[1]}&lon=${results1[0]}&appid=${openWeatherAppId}&units=imperial`).done(function (data) {
-            console.log(data)
+            // LOOPS THROUGH EACH 8TH ITEM OF THE 40 ITEMS IN DATA OBJECT
+            // RESULTING IN 5 DAY FORECAST FOR SEARCH BAR
             for (var i = 0; i <= 39; i += 8) {
 
                 let date = new Date((data.list[i].dt) * 1000);
                 let forecastDate = date.toLocaleDateString("en-US");
 
-                html += `<div class="col-2 card mx-2" style="color: #F0F0F0; font-family: Inter, sans-serif;">`
+                html += `<div class="col-2 card mx-2 fade-in-text" style="color: #F0F0F0; font-family: Inter, sans-serif;">`
 
                 html += `<img class='icons card-img-top' width="10" height="90" src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`
 
@@ -47,7 +47,7 @@ function geoCodeBuildForecast(searchString) {
             html += `</div>`
 
             $("#weatherBody").html(html);
-
+            //CURRENT LOCATION FOR SEARCH BAR
             let city = "";
             city += `<h4 class="d-flex justify-content-center align-content-center" style="font-family: Inter, sans-serif; color: #FB8500">Current Location: ${data.city.name}</h4>`
             $("#city").html(city);
@@ -55,6 +55,8 @@ function geoCodeBuildForecast(searchString) {
         html = "";
     })
 }
+
+//CONNECTS SEARCH BAR TO FORECAST FUNCTION
 $("#weatherSearch").click(function (e) {
     e.preventDefault();
     geoCodeBuildForecast($("#searchInput").val());
@@ -73,14 +75,13 @@ const map = new mapboxgl.Map({
     zoom: 12, // starting zoom
 });
 
-
-
 const marker = new mapboxgl.Marker({
     draggable: true
 })
     marker.setLngLat([ -98.48008626235448, 29.425988469224475])
     marker.addTo(map);
 
+//MARKER DRAGGABLE ON MAP, BELOW FUNCTION GATHERS DATA FROM MARKER LOCATION
 marker.on("dragend", function(e) {
      let html="";
 
@@ -88,7 +89,8 @@ marker.on("dragend", function(e) {
 
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lng
         }&appid=${openWeatherAppId}&units=imperial`).done(function (data){
-
+            // LOOPS THROUGH EACH 8TH ITEM OF THE 40 ITEMS IN DATA OBJECT
+            // RESULTING IN 5 DAY FORECAST FOR MARKER
             for (var i = 0; i <= 39; i += 8) {
 
                 let date = new Date((data.list[i].dt) * 1000);
@@ -124,7 +126,7 @@ marker.on("dragend", function(e) {
 
 
             $("#weatherBody").html(html);
-
+            //CURRENT LOCATION FOR MARKER
             let city = "";
             city += `<h4 class="d-flex justify-content-center align-content-center" style="font-family: Inter, sans-serif; color: #FB8500">Current Location: ${data.city.name}</h4>`
             $("#city").html(city);
@@ -132,22 +134,9 @@ marker.on("dragend", function(e) {
         })
     })
 
-
+//CALLS FORECAST FUNCTION AS DEFAULT UPON OPENING/RELOADING WINDOW
 geoCodeBuildForecast("San Antonio, TX");
 
-// function geoCodeBuildPollution (searchString) {
-//     let html = "";
-//     geocode(searchString, mapBoxAppId).then(function (results) {
-//         console.log(results);
-//         $.get(`http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherAppId}`).done(function (data){
-//             alert(data)
-//         })
-//         $("#weatherBody").html(html);
-//     })
-//     $("#weatherSearch").click(function (e) {
-//         e.preventDefault();
-//         geoCodeBuildForecast($("#searchInput").val());
-//     })
-// }
+
 
 
